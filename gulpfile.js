009f2396eab9108806f7d9ae626a4d63b68dbcae
Franck Martin <fmartin@runmyprocess.com>
2017-09-21 17:57:43 +0200

    Corrected linting problems. Completed gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -5,63 +5,78 @@
  */
 
 const gulp = require('gulp')
-const postcss = require('gulp-postcss')
-const autoprefixer = require('autoprefixer')
-const cssnano = require('cssnano')
-const stylelint = require('stylelint')
-const stylelintConfigStandard = require('stylelint-config-standard')
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const cssnano = require('gulp-cssnano')
+
+// Linter
+const stylelint = require('gulp-stylelint')
+const formatter = require('stylelint-formatter-pretty')
+const stylefmt = require('gulp-stylefmt');
 
 // Utilities
+const sequence = require('gulp-sequence')
 const rename = require('gulp-rename')
 const gutil = require('gulp-util')
 
+// SASS Linting
+gulp.task('lint-sass', function () {
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(stylelint({
+      reporters: [
+        {formatter: formatter, console: true}
+      ]
+    }));
+});
 
-gulp.task('sass:delivery', function () {
-  return gulp.src('src/sass/delivery/delivery.scss')
+// SASS APP Compilation
+gulp.task('sass-app', function () {
+  return gulp.src('src/sass/delivery-app/main.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['last 1 version']}))
+    .pipe(rename("css/runmyprocess-delivery-app.css"))
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('sass:homepage', function () {
-  return gulp.src('src/sass/homepage/home.scss')
+// SASS HOME Compilation
+gulp.task('sass-home', function () {
+  return gulp.src('src/sass/homepage/main.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['last 1 version']}))
+    .pipe(rename("css/runmyprocess-delivery-home.css"))
     .pipe(gulp.dest('./dist'));
 });
 
+/*
 gulp.task('sass:mobile', function () {
   return gulp.src('src/sass/mobile/mobile.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist'));
 });
-
-gulp.task('sass:watch', function () {
-  gulp.watch('src/sass/**/*.scss', ['sass']);
-});
-
-// CSS Building
-gulp.task('build', function () {
-  var plugins = [
-    autoprefixer({browsers: ['last 1 version']}),
-    cssnano()
-  ]
-  return gulp.src('src/css/*.css')
-  .pipe(postcss(plugins))
-  .pipe(rename(function (path) {
-    path.basename += '.min'
-  }))
-  .pipe(gulp.dest('./dist'));
- })
+*/
 
 // CSS Linting
-gulp.task('lint', function () {
-  var plugins = [
-    stylelint()
-  ]
-  return gulp.src('src/css/*.css')
-  .pipe(postcss(plugins))
-})
+gulp.task('lint-css', function () {
+  return gulp.src('dist/css/*.css')
+    .pipe(stylefmt())
+    .pipe(stylelint({
+      failAfterError: false,
+      reporters: [
+        {formatter: formatter, console: true}
+      ]
+    }));
+});
 
-gulp.task('default', ['build'], function () {
-  // This will only run if the lint task is successful...
-})
+// CSS Minification
+gulp.task('minify', function () {
+  return gulp.src('dist/css/*.css')
+    .pipe(cssnano())
+    .pipe(rename(function (path) {
+      path.dirname = 'css/min'
+      path.basename += '.min'
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+// BUILD Process
+gulp.task('default', sequence('lint-sass', ['sass-app', 'sass-home'], 'lint-css', 'minify'))
