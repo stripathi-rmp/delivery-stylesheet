e6082fea9cb1a1eba3f89bb676386fe618b35f04
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-31 08:47:01 +0000

    Deploy runmyprocess/delivery-stylesheet to github.com/runmyprocess/delivery-stylesheet.git:gh-pages

---
+++ README.md
@@ -0,0 +1,22 @@
+# Delivery Stylesheet
+
+## Presentation
+
+This templeate is intended to be used  by RMP's Delivery Team as an additional CSS layer on top of portal2 CSS.
+
+__Version: 1.0__
+
+Copyright (c) [Fujitsu RunMyProcess](https://www.runmyprocess.com/) - 2017
+
+## Authors
+-   [David Courtaigne](mailto:dcourtaigne@runmyprocess.com)
+-   [Quentin Boussard](mailto:qboussard@runmyprocess.com)
+-   [Rolando Fernandez](mailto:rfernandez@runmyprocess.com)
+
+## Documentation
+
+[Delivery Stylesheet documentation on Confluence](https://runmyprocess.atlassian.net/wiki/spaces/stylesheet/)
+
+## Download
+
+[Delivery Stylesheet](dist/delivery.css)
---
+++ gulpfile.js
@@ -0,0 +1,145 @@
+/**
+ * Gulp configuration file
+ * For convenience all the Gulp tasks are aliased in Npm
+ * Tasks : lint-sass, sass, lint-css, minify, backup, doc
+ */
+
+// Main
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+//const cssnano = require('gulp-cssnano')
+const cleancss = require('gulp-clean-css')
+
+// Linter
+const stylelint = require('gulp-stylelint')
+const formatter = require('stylelint-formatter-pretty')
+//const stylefmt = require('gulp-stylefmt')
+
+// Documentation
+const template = require('gulp-template')
+const fs = require('fs')
+
+// Utilities
+const rename = require('gulp-rename')
+const del = require('del')
+
+// Data
+const packageJSON = require('./package.json')
+const prefix = 'runmyprocess-delivery'
+
+
+// SASS LINTING
+gulp.task('lint-sass', function () {
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(stylelint({
+      failAfterError: false,
+      reporters: [
+        {formatter: formatter, console: true}
+      ]
+    }));
+});
+
+// SASS COMPILATION
+
+gulp.task('sass-home', function () {
+  del(['dist/css/tmp/*-home-*.css'])
+  console.log("Home css removed")
+  return gulp.src('src/sass/home/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['last 1 version']}))
+    .pipe(rename(function (path) {
+      path.basename = prefix + '-home-' + packageJSON.versionHomepage
+      path.extname = ".css"
+      path.dirname = ''
+      console.log("Generation -> ", path.basename)
+    }))
+    .pipe(gulp.dest('dist/css/tmp'))
+
+});
+
+gulp.task('sass-app', function () {
+  del(['dist/css/tmp/*-app-*.css'])
+  console.log("App css removed")
+  return gulp.src('src/sass/app/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['last 1 version']}))
+    .pipe(rename(function (path) {
+      path.basename = prefix + '-app-' + packageJSON.versionApp
+      path.extname = ".css"
+      path.dirname = ''
+      console.log("Generation -> ", path.basename)
+    }))
+    .pipe(gulp.dest('dist/css/tmp'))
+});
+
+gulp.task('sass-mobile', function () {
+  console.log("Mobile css removed")
+  del(['dist/css/tmp/*-mobile-*.css'])
+  return gulp.src('src/sass/mobile/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['last 1 version']}))
+    .pipe(rename(function (path) {
+      path.basename = prefix + '-mobile-' + packageJSON.versionMobile
+      path.extname = ".css"
+      path.dirname = ''
+      console.log("Generation -> ", path.basename)
+    }))
+    .pipe(gulp.dest('dist/css/tmp'))
+});
+
+gulp.task('sass', gulp.series('sass-home','sass-app','sass-mobile'));
+
+// CSS LINTING
+gulp.task('lint-css', function () {
+  return gulp.src('dist/css/tmp/*.css')
+    //.pipe(stylefmt())
+    .pipe(stylelint({
+      fix: true,
+      failAfterError: false,
+      reporters: [
+        {formatter: formatter, console: true}
+      ]
+    }))
+    .pipe(gulp.dest('./dist/css'))
+})
+
+// CSS MINIFICATION
+gulp.task('minify', function () {
+  return gulp.src('dist/css/tmp/*.css')
+    //.pipe(cssnano())
+    //.pipe(cleancss({compatibility: 'ie8'}))
+    .pipe(cleancss({level: 2}))
+    .pipe(rename(function (path) {
+      path.basename += '.min'
+    }))
+    .pipe(gulp.dest('./dist/css'))
+});
+
+// CSS BACKUP
+gulp.task('backup', () =>
+  gulp.src('dist/css/*.css')
+  .pipe(gulp.dest('./dist/css/archive'))
+  .pipe(gulp.dest('./backup'))
+)
+
+// CSS DOCUMENTATION
+gulp.task('doc', () =>
+  gulp.src('src/doc.html')
+    .pipe(template({
+      description: packageJSON.description,
+      versionApp: packageJSON.versionApp,
+      versionHomepage: packageJSON.versionHomepage,
+      versionMobile: packageJSON.versionMobile,
+      contributors: packageJSON.contributors,
+      homepage: packageJSON.homepage,
+      bugs: packageJSON.bugs,
+      archives: fs.readdirSync('dist/css/archive/'),
+      prefix: prefix
+    }))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('./'))
+)
+
+// BUILD
+gulp.task('default', gulp.series('lint-sass', 'sass', 'lint-css', 'minify', 'backup', 'doc'))
